2175365a77315799beb5d47598cb73eb360aa6e4
SJDeane <165832267+SJDeane@users.noreply.github.com>
2024-04-03 20:05:14 +0100

    Move program to single core

--- main/main.ino
+++ main/main.ino
@@ -28,8 +28,9 @@ Servo shifter;
 #define Low_PWM_Trigger 0.3
 
 // PWM
-volatile std::array<unsigned long, 2> isrPulsewidth = {0, 0};     // Define Interrupt Service Routine (ISR) pulsewidth
-unsigned std::array<long, 2> pulseWidth = {0, 0};                 // Define Pulsewidth variable
+std::mutex pwm_lock_src_1, pwm_lock_src_2;
+std::array<unsigned long, 2> isrPulsewidth = {0, 0};     // Define Interrupt Service Routine (ISR) pulsewidth
+std::array<unsigned long, 2> pulseWidth = {0, 0};                 // Define Pulsewidth variable
 
 // shared parameter
 std::mutex global_mutex;
@@ -40,15 +41,15 @@ void calcPulsewidth_src1()
 {
   static unsigned long pulseStartTime_src_1;   // Start time variable
   
-  if (digitalRead(INPUT_PIN) == HIGH)    // If the change was a RISING edge
+  if (digitalRead(PWM_SRC_1) == HIGH)    // If the change was a RISING edge
   {
     pulseStartTime_src_1 = micros();           // Store the start time (in microseconds)
-    lastState_src_1 = {true};
   }
   else                                   // If the change was a FALLING edge
-  {        
+  {
+    pwm_lock_src_1.lock();
     isrPulsewidth[0] = micros() - pulseStartTime_src_1;    // Calculate the pulsewidth
-    lastState_src_1 = {false};
+    pwm_lock_src_1.unlock();
   }
 }
 
@@ -57,13 +58,15 @@ void calcPulsewidth_src2()
 {
   static unsigned long pulseStartTime_src_2;   // Start time variable
   
-  if (digitalRead(INPUT_PIN) == HIGH)    // If the change was a RISING edge
+  if (digitalRead(PWM_SRC_2) == HIGH)    // If the change was a RISING edge
   {
     pulseStartTime_src_2 = micros();           // Store the start time (in microseconds)
   }
   else                                   // If the change was a FALLING edge
-  {        
+  {
+    pwm_lock_src_2.lock();
     isrPulsewidth[1] = micros() - pulseStartTime_src_2;    // Calculate the pulsewidth
+    pwm_lock_src_2.unlock();
   }
 }
 
@@ -73,12 +76,12 @@ front diff  -> 0x00 00 00 0F
 rear diff   -> 0x00 00 00 F0
 shifter     -> 0x00 00 0F 00
 */
-void handle_signal(uint32_t *signal){
+void handle_signal(uint32_t signal){
   /*############################################################################*/
   /*#                           front diff control                             #*/
   /*############################################################################*/
-  if (*signal & 0x0000000F != 0x00000000){
-    uint8_t value = uint8_t(*signal & 0x0000000F);
+  if ((signal & 0x0000000F) != 0x00000000){
+    uint8_t value = uint8_t(signal & 0x0000000F);
     switch (value) {
       case 1:
         frontDiff.write(servo_triggered);
@@ -86,15 +89,15 @@ void handle_signal(uint32_t *signal){
       case 2:
         frontDiff.write(servo_centured);
       default:
-        continue;
+        break;
     }
-    *signal = *signal & !0x0000000F;
+    signal = signal & !0x0000000F;
   }
   /*############################################################################*/
   /*#                           rear diff control                              #*/
   /*############################################################################*/
-  if (*signal & 0x000000F0 == 0x00000000){
-    uint8_t value = uint8_t((*signal & 0x000000F0) >> (1 * hex_size));
+  if ((signal & 0x000000F0) == 0x00000000){
+    uint8_t value = uint8_t((signal & 0x000000F0) >> (1 * hex_size));
     switch (value) {
       case 1:
         rearDiff.write(servo_triggered);
@@ -103,15 +106,15 @@ void handle_signal(uint32_t *signal){
         rearDiff.write(servo_centured);
         break;
       default:
-        continue;
+        break;
     }
-    *signal = *signal & !0x000000F0;
+    signal = signal & !0x000000F0;
   }
   /*############################################################################*/
   /*#                             shifter control                              #*/
   /*############################################################################*/
-  if (*signal & 0x00000F00 == 0x00000000){
-    uint8_t value = uint8_t((*signal & 0x00000F00) >> (2 * hex_size));
+  if ((signal & 0x00000F00) == 0x00000000){
+    uint8_t value = uint8_t((signal & 0x00000F00) >> (2 * hex_size));
     switch (value) {
       case 1:
         shifter.write(servo_triggered);
@@ -120,35 +123,35 @@ void handle_signal(uint32_t *signal){
         shifter.write(servo_centured);
         break;
       default:
-        continue;
+        break;
     }
     /*############################################################################*/
   }
-  *signal = *signal & !0x00000F00;
+  signal = signal & !0x00000F00;
 }
 
 // Servo handler thread will control the position of all of the servos
-void servo_handler(void){
-  uint32_t local_signal = 0x00000000;
-  while(1){
-    global_mutex.lock();
-    if (global_signal != 0x00000000){
-      local_signal = global_signal;
-      global_signal = 0x00000000;
-    }
-    global_mutex.unlock();
-    if (local_signal != 0x00000000){
-      handle_signal(&local_signal);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-}
+// void servo_handler(void){
+//   uint32_t local_signal = 0x00000000;
+//   while(1){
+//     global_mutex.lock();
+//     if (global_signal != 0x00000000){
+//       local_signal = global_signal;
+//       global_signal = 0x00000000;
+//     }
+//     global_mutex.unlock();
+//     if (local_signal != 0x00000000){
+//       handle_signal(&local_signal);
+//     }
+//     std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//   }
+// }
 
-void send_signal_to_thread(uint8_t signal){
-  global_mutex.lock();
-  global_signal = signal;
-global_mutex.unlock();
-}
+// void send_signal_to_thread(uint8_t signal){
+//   global_mutex.lock();
+//   global_signal = signal;
+// global_mutex.unlock();
+// }
 
 void setup() {
   // Allow allocation of all timers
@@ -176,34 +179,38 @@ void setup() {
 void loop() {
   // interupt safe
   noInterrupts();                         // Turn off interrupts
+  pwm_lock_src_1.lock();
+  pwm_lock_src_2.lock();
   pulseWidth = isrPulsewidth;             // Copy the isr pulsewidth to the pulsewidth variable
+  pwm_lock_src_1.unlock();
+  pwm_lock_src_2.unlock();
   interrupts();                           // Turn on interrupts
 
   if (pulseWidth[1] >= High_PWM_Trigger || pulseWidth[1] <= Low_PWM_Trigger){
     // trigger control for second core
-    uint8_t selector = uint8_t(std::floor(pulseWidth[0] * 10)
+    uint8_t selector = uint8_t(std::floor(pulseWidth[0] * 10));
     if (selector > 0 && selector <= 3){
       if (pulseWidth[1] >= High_PWM_Trigger){
-        send_signal_to_thread(0x00000002);
+        handle_signal(0x00000002);
       }
       else {
-        send_signal_to_thread(0x00000001);
+        handle_signal(0x00000001);
       }
     }
     else if (selector > 3 && selector <= 6){
       if (pulseWidth[1] >= High_PWM_Trigger){
-        send_signal_to_thread(0x00000020);
+        handle_signal(0x00000020);
       }
       else {
-        send_signal_to_thread(0x00000010);
+        handle_signal(0x00000010);
       }
     }
     else if (selector > 6 && selector <= 9){
       if (pulseWidth[1] >= High_PWM_Trigger){
-        send_signal_to_thread(0x00000200);
+        handle_signal(0x00000200);
       }
       else {
-        send_signal_to_thread(0x00000100);
+        handle_signal(0x00000100);
       }
     }
 
