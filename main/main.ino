f83bd5cf5343b22fe83205892118cb2df10abb95
SJDeane <165832267+SJDeane@users.noreply.github.com>
2025-08-02 21:07:56 +0100

    Added HTML handler, wifi server and webpage

--- main/main.ino
+++ main/main.ino
@@ -2,9 +2,11 @@
 #include <thread>
 
 #include <ESP32Servo.h>
+#include <WiFi.h>
 
 #include "Arduino.h"
 
+/************************ Servo control ************************/
 // front diff object
 Servo frontDiff;
 #define frontDiffPin 27
@@ -38,43 +40,39 @@ Servo shifter;
 
 // PWM
 std::mutex pwm_lock_src_1, pwm_lock_src_2;
-std::array<unsigned long, 2> isrPulsewidth = {0, 0};     // Define Interrupt Service Routine (ISR) pulsewidth
-std::array<unsigned long, 2> pulseWidth = {0, 0};                 // Define Pulsewidth variable
+std::array<unsigned long, 2> isrPulsewidth = { 0, 0 };  // Define Interrupt Service Routine (ISR) pulsewidth
+std::array<unsigned long, 2> pulseWidth = { 0, 0 };     // Define Pulsewidth variable
 
 // shared parameter
 std::mutex global_mutex;
 uint32_t global_signal = 0x00000000;
 
 // Interrupt Service Routine (ISR)
-void calcPulsewidth_src1()                    
-{
-  static unsigned long pulseStartTime_src_1;   // Start time variable
-  
-  if (digitalRead(PWM_SRC_1) == HIGH)    // If the change was a RISING edge
+void calcPulsewidth_src1() {
+  static unsigned long pulseStartTime_src_1;  // Start time variable
+
+  if (digitalRead(PWM_SRC_1) == HIGH)  // If the change was a RISING edge
   {
-    pulseStartTime_src_1 = micros();           // Store the start time (in microseconds)
-  }
-  else                                   // If the change was a FALLING edge
+    pulseStartTime_src_1 = micros();  // Store the start time (in microseconds)
+  } else                              // If the change was a FALLING edge
   {
     pwm_lock_src_1.lock();
-    isrPulsewidth[0] = micros() - pulseStartTime_src_1;    // Calculate the pulsewidth
+    isrPulsewidth[0] = micros() - pulseStartTime_src_1;  // Calculate the pulsewidth
     pwm_lock_src_1.unlock();
   }
 }
 
 // Interrupt Service Routine (ISR)
-void calcPulsewidth_src2()                    
-{
-  static unsigned long pulseStartTime_src_2;   // Start time variable
-  
-  if (digitalRead(PWM_SRC_2) == HIGH)    // If the change was a RISING edge
+void calcPulsewidth_src2() {
+  static unsigned long pulseStartTime_src_2;  // Start time variable
+
+  if (digitalRead(PWM_SRC_2) == HIGH)  // If the change was a RISING edge
   {
-    pulseStartTime_src_2 = micros();           // Store the start time (in microseconds)
-  }
-  else                                   // If the change was a FALLING edge
+    pulseStartTime_src_2 = micros();  // Store the start time (in microseconds)
+  } else                              // If the change was a FALLING edge
   {
     pwm_lock_src_2.lock();
-    isrPulsewidth[1] = micros() - pulseStartTime_src_2;    // Calculate the pulsewidth
+    isrPulsewidth[1] = micros() - pulseStartTime_src_2;  // Calculate the pulsewidth
     pwm_lock_src_2.unlock();
   }
 }
@@ -85,11 +83,11 @@ front diff  -> 0x00 00 00 0F
 rear diff   -> 0x00 00 00 F0
 shifter     -> 0x00 00 0F 00
 */
-void handle_signal(uint32_t signal){
+void handle_signal(uint32_t signal) {
   /*############################################################################*/
   /*#                           front diff control                             #*/
   /*############################################################################*/
-  if ((signal & 0x0000000F) != 0x00000000){
+  if ((signal & 0x0000000F) != 0x00000000) {
     uint8_t value = uint8_t(signal & 0x0000000F);
     Serial.println("front diff value: " + String(value));
     switch (value) {
@@ -106,7 +104,7 @@ void handle_signal(uint32_t signal){
   /*############################################################################*/
   /*#                           rear diff control                              #*/
   /*############################################################################*/
-  if ((signal & 0x000000F0) != 0x00000000){
+  if ((signal & 0x000000F0) != 0x00000000) {
     uint8_t value = uint8_t((signal & 0x000000F0) >> (1 * hex_size));
     Serial.println("rear diff value: " + String(value));
     switch (value) {
@@ -116,7 +114,7 @@ void handle_signal(uint32_t signal){
       case 2:
         rearDiff.write(105);
         break;
-      default:  
+      default:
         break;
     }
     // signal = signal & !0x000000F0;
@@ -124,7 +122,7 @@ void handle_signal(uint32_t signal){
   /*############################################################################*/
   /*#                             shifter control                              #*/
   /*############################################################################*/
-  if ((signal & 0x00000F00) != 0x00000000){
+  if ((signal & 0x00000F00) != 0x00000000) {
     uint8_t value = uint8_t((signal & 0x00000F00) >> (2 * hex_size));
     Serial.println("Shifter value: " + String(value));
     switch (value) {
@@ -165,28 +163,54 @@ void handle_signal(uint32_t signal){
 // global_mutex.unlock();
 // }
 
+/************************ Wifi control ************************/
+
+const char* wifi_name = "ESP32-Access-Point";
+const char* password = "123456789";
+WiFiServer server(80);
+bool client_connected = false;
+WiFiClient client;
+String currentLine = "";
+
+String HTML_print(String message){
+  return "<p>" + message + "</p>";
+}
+String Add_HTML_Button(String message, String active_message, uint position){
+  return "<form id='F" + String(position) + "' action='" + active_message + "'><input class='button' type='submit' value='" + message + "' ></form><br>";
+}
+
+
+String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+String html_1 = "<!DOCTYPE html><html><head><title>LED Control</title></head><body><div id='main'><h2>LED Control</h2>";
+String html_4 = "</div></body></html>";
+
+/************************ Main ************************/
 void setup() {
   // Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2); 
-	Serial.begin(115200);
-	frontDiff.setPeriodHertz(50);      // Standard 50hz servo
-	rearDiff.setPeriodHertz(50);      // Standard 50hz servo
-	shifter.setPeriodHertz(50);      // Standard 50hz servo
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  Serial.begin(115200);
+  frontDiff.setPeriodHertz(50);  // Standard 50hz servo
+  rearDiff.setPeriodHertz(50);   // Standard 50hz servo
+  shifter.setPeriodHertz(50);    // Standard 50hz servo
 
   frontDiff.attach(frontDiffPin, 1000, 2000);
-	rearDiff.attach(rearDiffPin, 1000, 2000);
-	shifter.attach(shifterPin, 1000, 2000);
+  rearDiff.attach(rearDiffPin, 1000, 2000);
+  shifter.attach(shifterPin, 1000, 2000);
 
   // zero all servos
   frontDiff.write(105);
-	rearDiff.write(105);
-	shifter.write(90);
+  rearDiff.write(105);
+  shifter.write(90);
 
-  attachInterrupt(digitalPinToInterrupt(PWM_SRC_1), calcPulsewidth_src1, CHANGE);   // Run the calcPulsewidth function on signal CHANGE
-  attachInterrupt(digitalPinToInterrupt(PWM_SRC_2), calcPulsewidth_src2, CHANGE);   // Run the calcPulsewidth function on signal CHANGE
-  
+  attachInterrupt(digitalPinToInterrupt(PWM_SRC_1), calcPulsewidth_src1, CHANGE);  // Run the calcPulsewidth function on signal CHANGE
+  attachInterrupt(digitalPinToInterrupt(PWM_SRC_2), calcPulsewidth_src2, CHANGE);  // Run the calcPulsewidth function on signal CHANGE
+  WiFi.softAP(wifi_name, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  server.begin();
 }
 
 /*
@@ -195,61 +219,110 @@ Signal voltage = 3.3v
 
 */
 void loop() {
+  /*********** Servo handler ***********/
   // interupt safe
-  noInterrupts();                         // Turn off interrupts
+  noInterrupts();  // Turn off interrupts
   pwm_lock_src_1.lock();
   pwm_lock_src_2.lock();
-  pulseWidth = isrPulsewidth;             // Copy the isr pulsewidth to the pulsewidth variable
+  pulseWidth = isrPulsewidth;  // Copy the isr pulsewidth to the pulsewidth variable
   pwm_lock_src_1.unlock();
   pwm_lock_src_2.unlock();
-  interrupts();                           // Turn on interrupts
-  Serial.print(pulseWidth[0] - 1000);
-  Serial.print(" ");
-  Serial.println(pulseWidth[1] - 1000);
-  if (pulseWidth[1] >= High_PWM_Trigger || pulseWidth[1] <= Low_PWM_Trigger){
+  interrupts();  // Turn on interrupts
+  // Serial.print(pulseWidth[0] - 1000);
+  // Serial.print(" ");
+  // Serial.println(pulseWidth[1] - 1000);
+  if (pulseWidth[1] >= High_PWM_Trigger || pulseWidth[1] <= Low_PWM_Trigger) {
     // trigger control for second core
-    
-    unsigned long selector = ((pulseWidth[0]  - 1000));
-    Serial.println("Range 1: " + String(selector > 525) + "-" + String(selector <= 575) + "\t selector value: " + String(selector));
-    if ((selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * FRONT_DIFF_POINT))) && (selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (FRONT_DIFF_POINT + 1))))){
+
+    unsigned long selector = ((pulseWidth[0] - 1000));
+    // Serial.println("Range 1: " + String(selector > 525) + "-" + String(selector <= 575) + "\t selector value: " + String(selector));
+    if ((selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * FRONT_DIFF_POINT))) && (selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (FRONT_DIFF_POINT + 1))))) {
       // Serial.println("Front Diff");
-      if (pulseWidth[1] >= High_PWM_Trigger){
+      if (pulseWidth[1] >= High_PWM_Trigger) {
         handle_signal(0x00000002);
-      }
-      else {
+      } else {
         handle_signal(0x00000001);
       }
-    }
-    else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * REAR_DIFF_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (REAR_DIFF_POINT + 1)))){
+    } else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * REAR_DIFF_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (REAR_DIFF_POINT + 1)))) {
       Serial.println("Rear Diff");
-      if (pulseWidth[1] >= High_PWM_Trigger){
-        
+      if (pulseWidth[1] >= High_PWM_Trigger) {
+
         handle_signal(0x00000020);
-      }
-      else {
+      } else {
         handle_signal(0x00000010);
       }
-    }
-    else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * SHIFTER_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (SHIFTER_POINT + 1)))){
+    } else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * SHIFTER_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (SHIFTER_POINT + 1)))) {
       // Serial.println("Shifter");
-      if (pulseWidth[1] >= High_PWM_Trigger){
+      if (pulseWidth[1] >= High_PWM_Trigger) {
         handle_signal(0x00000200);
-      }
-      else {
+      } else {
         handle_signal(0x00000100);
       }
-    }
-    else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * BOTH_DIFF_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (BOTH_DIFF_POINT + 1)))){
+    } else if (selector > (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * BOTH_DIFF_POINT)) && selector <= (SERVO_PWM_CENTURE + (SIGNAL_STEP_SIZE * (BOTH_DIFF_POINT + 1)))) {
       // Serial.println("Both Diffs");
-      if (pulseWidth[1] >= High_PWM_Trigger){
+      if (pulseWidth[1] >= High_PWM_Trigger) {
         handle_signal(0x00000022);
-      }
-      else {
+      } else {
         handle_signal(0x00000011);
       }
     }
-
   }
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+  /**************** Wifi handler ******************/
+  if (!client_connected) {
+    client = server.accept();
+    if (client) {
+      // Serial.println("New Client.");
+      client_connected = true;
+    }
+  } 
+  if (client_connected && client) {
+    if (client.connected() && client.available()) {
+      char c = client.read();  // read a byte, then
+      // Serial.write(c);         // print it out the serial monitor
+      currentLine += c;
+      if (c == '\n') {  // if the byte is a newline character
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 2) {
+          String temp_reading = ("Temp: " + String(temperatureRead()) + "°C");
+          // Send HTML data
+          client.clear();
+          client.print( header );
+          client.print( html_1 );
+          client.print( Add_HTML_Button("Front Diff On", "FDIFFON", 1) );
+          client.print( Add_HTML_Button("Front Diff Off", "FDIFFOFF", 1) );
+          client.print(HTML_print(temp_reading));
+          client.print( html_4 );
+          client.stop();
+          client_connected = false;
+        } else {  // if you got a newline, then clear currentLine
+          currentLine = "";
+        }
+      } else if (c != '\r') {  // Monitor for HTML return
+        // Serial.println("Command: " + currentLine);
+        
+        
+        if       ( currentLine.indexOf("FDIFFON") > 0 )  { 
+          Serial.println("#############\nEngaging front Diff\n#############");  
+          currentLine = ""; // clear the message to prevent double triggers
+        }
+        else if  ( currentLine.indexOf("FDIFFOFF") > 0 ) { 
+          Serial.println("#############\nDissabeling front Diff\n#############"); 
+          currentLine = ""; // clear the message to prevent double triggers
+        }
+        
+      }
+    } else {
+      // Serial.println("Client Closed.");
+      client.stop();
+      client_connected = false;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    } 
+  } else if (client_connected && !client) {
+    client_connected = false;
+  } else {
+    // Serial.println(("Temp: " + std::to_string(temperatureRead()) + "°C").c_str());
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  
 }
